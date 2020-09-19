94c65b5b92a2771c723d8f4d73503ebd2d5b37f8
Rahul Pushpala <53091671+T3ch-miNer@users.noreply.github.com>
2020-09-19 15:14:56 +0530

    deployit

---
+++ bin/preprocessor.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 19 13:03:41 2020
+
+@author: rahul
+"""
+import re 
+
+class ProcessDoc:
+    """
+    Module for preprocessing arcticles
+    """
+    def removeSpecChar():
+        pass
+    
+    def tokeniz():
+        pass
+    
+    def stopword_remve():
+        pass
---
+++ bin/summarizer.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 19 13:13:42 2020
+
+@author: rahul
+"""
+
+
