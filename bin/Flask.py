f4b07d25b78f5932a3f2680ebdd884d7500b78f9
Rahul Pushpala <53091671+T3ch-miNer@users.noreply.github.com>
2020-09-26 16:28:07 +0530

    update itsss

--- bin/Flask.py
+++ bin/Flask.py
@@ -13,9 +13,10 @@ from summarizer import SummarizeDoc
 app = Flask(__name__)
 
 @app.route('/get_summary',methods=['GET'])
+
 def findSummary():
     summarizeObj = SummarizeDoc()
     summary = summarizeObj.findSummary()
     return summary
 
-app.run('localhost',8023)
+app.run('localhost',5000)
